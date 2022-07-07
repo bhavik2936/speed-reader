8a7a1dc05da3b31c1ea6f79cd2642786431d0404
Bhavik Parmar <38880921+bhavik2936@users.noreply.github.com>
2022-07-08 01:05:13 +0530

    add react support

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const webpack = require('webpack');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 const isProd = process.env.NODE_ENV === 'production';
 const isDev = !isProd;
@@ -34,17 +36,30 @@ const config = {
     ]
   },
 
+  plugins: [
+    // Generate index.html on base of src/index.html
+    new htmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+
+    // Allows libraries to be used without requiring an `import` or `require()` call.
+    new webpack.ProvidePlugin({
+      React: 'react',
+    }),
+  ],
+
   // Allows us to import modules without needing to add their extensions
   resolve: {
     modules: ['src', 'node_modules'],
-    extensions: ['*', 'js', 'jsx']
+    extensions: ['*', '.js', '.jsx']
   },
 
   output: {
     // Delete previous builds when starting a new build
     clean: true,
     // Base path for asset URL links
-    publicPath: '/dist/',
+    publicPath: '/',
     // Destination for built files
     path: path.resolve(__dirname, 'dist'),
     // Name pattern for build files
@@ -70,22 +85,28 @@ const config = {
         }
       }
     }
-  }
+  },
 }
 
 if (isDev) {
   const host = 'localhost';
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 8080;
 
-  config.output.publicPath = `http://${host}:${port}/dist/`;
+  config.output.publicPath = `http://${host}:${port}/`;
   config.devServer = {
     port: port,
     host: host,
     // Enable Hot Module Replacement (HMR)
     hot: true,
 
-    contentBase: path.resolve(__dirname, 'public'),
-  }
+    static: {
+      directory: path.resolve(__dirname, 'public'),
+    },
+
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  };
 }
 
 module.exports = config;
