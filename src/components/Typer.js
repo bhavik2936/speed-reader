e9446abb782d5ce15a71a195396d850ab909bf2d
Bhavik Parmar <38880921+bhavik2936@users.noreply.github.com>
2023-12-08 17:08:30 +0530

    optimize and removed redundant logic
    Co-authored-by: Kishan Aghera <55836816+kishan-aghera@users.noreply.github.com>

--- src/components/Typer.js
+++ src/components/Typer.js
@@ -1,25 +1,24 @@
-import { useState } from "react";
-
-const Typer = ({isReading, para, setParaState}) => {
-  const [paragraph, setParagraph] = useState(para);
-
+const Typer = ({ paragraph, setParagraph, startReading }) => {
   const handleSubmit = (event) => {
-    // prevent the form from submitting
+    // Prevents the form from submitting
     event.preventDefault();
-    setParaState(paragraph);
-  }
+    startReading();
+  };
 
   return (
-    isReading ? null : (
-      <form onSubmit={handleSubmit}>
-        <div>
-          <textarea value={paragraph} rows="10" cols="50" onChange={(e) => setParagraph(e.target.value)} />
-        </div>
-        <div>
-          <input type="submit" value="Speed It" />
-        </div>
-      </form>
-    )
+    <form onSubmit={handleSubmit}>
+      <div>
+        <textarea
+          value={paragraph}
+          rows="10"
+          cols="50"
+          onChange={(e) => setParagraph(e.target.value)}
+        />
+      </div>
+      <div>
+        <input type="submit" value="Read" />
+      </div>
+    </form>
   );
 };
 
